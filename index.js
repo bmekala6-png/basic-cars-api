9e423810b92eb63029cb0a8476b4b2cfb57b8d46
bmekala6-png <bmekala6@gmail.com>
2026-01-30 23:25:32 +0530

    second-commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const Car = require('./models/carsModels');
@@ -15,14 +16,11 @@ app.get('/',(req,res)=>{
     res.send("you are in the cars webpage");
 })
 
-
-mongoose.connect(
-    "mongodb+srv://balaiahyadav12_db_user:1RneFoMBLxomCFVc@cluster0.azdlzed.mongodb.net/?appName=Cluster0"
-)
+mongoose.connect(process.env.MONGODB_URL)
 .then(()=>{
     console.log('connected to db');
-    app.listen('2001',(req,res)=>{
-        console.log("App is listening to the port: 2001");
+    app.listen(process.env.PORT,(req,res)=>{
+        console.log(`App is listening to the port: ${process.env.PORT}`);
 });
 })
 .catch((error)=>{
